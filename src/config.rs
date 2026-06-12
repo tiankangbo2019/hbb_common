61bbe0d72c7eb99e88482834814e9237b311d466
tiankangbo@outlook.com <tiankangbo@outlook.com>
2026-06-12 22:44:00 +0800

    47.100.164.197

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.100.164.197"];
+pub const RS_PUB_KEY: &str = "ISJ7iPUNx3zPTqrzoP2qIVCUcXj0e8RE8HmSz9RYgyo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
