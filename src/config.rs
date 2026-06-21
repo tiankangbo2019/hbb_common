d02b4deb9ccf2f909ca12c3d071051dd15886f6d
tiankangbo@outlook.com <tiankangbo@outlook.com>
2026-06-21 17:19:04 +0800

    revert: use 47.100.164.197 as rendezvous server

--- src/config.rs
+++ src/config.rs
@@ -117,7 +117,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["redesk.asia"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.100.164.197"];
 pub const RS_PUB_KEY: &str = "ISJ7iPUNx3zPTqrzoP2qIVCUcXj0e8RE8HmSz9RYgyo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
